cff0ed4d40756fca63ce0884b457e1a610da4e30
Lokeyokeria <lokeyokeria.maria@gmail.com>
2026-03-23 20:30:18 +0100

    Create PerfumesDuraderos.tsx

---
+++ pages/PerfumesDuraderos.tsx
@@ -0,0 +1,8 @@
+export default function PerfumesDuraderos() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Perfumes de equivalencia que más duran</h1>
+      <p>Descubre los perfumes con mayor fijación y proyección. Huelen todo el día.</p>
+    </div>
+  );
+}
