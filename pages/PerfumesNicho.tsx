8866c2555c34aa327f6e186aa611487b10741e73
Lokeyokeria <lokeyokeria.maria@gmail.com>
2026-03-23 20:29:54 +0100

    Create PerfumesNicho.tsx

---
+++ pages/PerfumesNicho.tsx
@@ -0,0 +1,8 @@
+export default function PerfumesNicho() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Perfumes nicho de equivalencia</h1>
+      <p>Fragancias únicas, diferentes y con personalidad para quienes buscan algo exclusivo.</p>
+    </div>
+  );
+}
