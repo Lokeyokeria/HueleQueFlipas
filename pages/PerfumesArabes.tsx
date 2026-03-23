978b2b56ba498926e285c142cd4d6fc46be794e8
Lokeyokeria <lokeyokeria.maria@gmail.com>
2026-03-23 20:29:17 +0100

    Create PerfumesArabes.tsx

---
+++ pages/PerfumesArabes.tsx
@@ -0,0 +1,8 @@
+export default function PerfumesArabes() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Perfumes árabes de equivalencia</h1>
+      <p>Aromas intensos, exóticos y con una duración brutal que no pasan desapercibidos.</p>
+    </div>
+  );
+}
