5f59791ffd74b26c64e2e375e2bef6233e765374
Lokeyokeria <lokeyokeria.maria@gmail.com>
2026-03-23 20:28:23 +0100

    Create PerfumesHombre.tsx

---
+++ pages/PerfumesHombre.tsx
@@ -0,0 +1,8 @@
+export default function PerfumesHombre() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Perfumes de equivalencia para hombre</h1>
+      <p>Fragancias intensas, elegantes y duraderas que huelen a lujo sin pagar de más.</p>
+    </div>
+  );
+}
