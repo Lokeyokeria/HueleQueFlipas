1c3e1b2c3823d6f0c8cede912daa979986208e6c
Lokeyokeria <lokeyokeria.maria@gmail.com>
2026-03-23 20:28:54 +0100

    Create PerfumesMujer.tsx

---
+++ pages/PerfumesMujer.tsx
@@ -0,0 +1,8 @@
+export default function PerfumesMujer() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Perfumes de equivalencia para mujer</h1>
+      <p>Perfumes irresistibles, femeninos y duraderos con calidad premium.</p>
+    </div>
+  );
+}
