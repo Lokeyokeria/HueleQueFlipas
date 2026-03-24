e3b8f894f41a758ba8479d03c625835f8594375c
Lokeyokeria <lokeyokeria.maria@gmail.com>
2026-03-24 13:45:16 +0100

    Create PerfumesUnisex.tsx

---
+++ pages/PerfumesUnisex.tsx
@@ -0,0 +1,8 @@
+export default function PerfumesUnisex() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Perfumes unisex</h1>
+      <p>Descubre perfumes unisex de equivalencia con personalidad, equilibrio y mucha presencia.</p>
+    </div>
+  );
+}
