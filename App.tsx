d841f15ceb43d0cd86f2d8f35ba073c8807a5eb1
Lokeyokeria <lokeyokeria.maria@gmail.com>
2026-03-25 18:17:02 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -903,8 +903,8 @@ const App: React.FC = () => {
               </h2>
 
               <p className="text-gray-500 max-w-2xl mx-auto font-medium text-base md:text-lg">
-                En la home te enseñamos solo una selección para que todo se vea más limpio. Dentro
-                de cada categoría podrás ver todos los perfumes disponibles.
+                En la home te enseñamos solo los más vendidos.   
+                   Dentro de cada categoría podrás ver todos los perfumes disponibles.
               </p>
             </div>
 
